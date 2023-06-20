c47c122fe4ccba9f363825646f0bac9d17eca039
Jiang <130443333+Bluskarc@users.noreply.github.com>
2023-06-20 18:04:08 +0900

    Add files via upload

--- hayame_test/main.dart
+++ hayame_test/main.dart
@@ -3,7 +3,7 @@ import 'interface.dart' as ui;
 import 'function.dart';
 
 void main() {
-  print(db.task_DB);
-  change_task_title(ui.get_id_UI(), ui.get_title_UI());
-  print(db.task_DB);
+  print(db.task_DB); //実際にはUIで表示する
+  change_task_title(ui.get_id_UI(), ui.get_title_UI()); //実際にはあるボタンを押す
+  print(db.task_DB); //実際にはUIで表示する
 }
