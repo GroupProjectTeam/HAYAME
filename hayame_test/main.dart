5e06c0edc0c5aface0cfd4e89e0d9c8c82b55eee
Jiang <130443333+Bluskarc@users.noreply.github.com>
2023-07-17 12:54:32 +0900

    Delete hayame_test directory

--- hayame_test/main.dart
+++
@@ -1,9 +0,0 @@
-import 'database.dart' as db;
-import 'interface.dart' as ui;
-import 'function.dart';
-
-void main() {
-  print(db.task_DB); //実際にはUIで表示する
-  change_task_title(ui.get_id_UI(), ui.get_title_UI()); //実際にはあるボタンを押す
-  print(db.task_DB); //実際にはUIで表示する
-}
